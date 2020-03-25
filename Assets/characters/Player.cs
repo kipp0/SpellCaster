590b8e178e24c608cefbf97faeecb4eba77f3a2f
Pierre Smith <smith.pierre@me.com>
2020-03-25 13:08:05 -0400

    start

---
+++ Assets/characters/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    static Animator a;
+    public float speed = 1.0f;
+    public float rotationSpeed = 100.0f;
+    
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        float translation = movement.y * speed;
+        float rotation = movement.x * rotationSpeed;
+
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0,0,translation);
+        transform.Rotate(0,rotation,0);
+        
+        a.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            a.SetTrigger("isJumping");
+            Debug.Log("jump");
+        }
+
+        if (translation > 0.01 && translation < 3)
+        {
+            
+        }
+    }
+}
