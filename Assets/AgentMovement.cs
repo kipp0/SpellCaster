de58628ae5d3819b419905bfb77c32e951141136
Pierre Smith <smith.pierre@me.com>
2020-04-19 13:54:19 -0400

    new start

---
+++ Assets/AgentMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMovement : MonoBehaviour
+{
+    [SerializeField] Transform target;
+       private NavMeshAgent agent;
+       private void Start() {
+            agent = GetComponent<NavMeshAgent>();
+            if (agent != null) { 
+                agent.SetDestination(target.position);
+            } 
+        }
+}
